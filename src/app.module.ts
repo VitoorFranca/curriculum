6023d3c8983230f55b26f659b97c66847265516b
Vitor França <franc.vitoor@gmail.com>
2021-08-03 17:46:49 -0300

    MongoDB & .envs & +

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+  // IMPORTS
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports:
+    [
+      ConfigModule.forRoot(),
+      MongooseModule.forRoot(
+        `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASS}@cluster0.jhzfs.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`,
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+        }
+      ),
+    ],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
